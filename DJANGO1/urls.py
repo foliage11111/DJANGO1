301a72302a316a88e5e570b15ea80c290309aa6a
foliage11111 <378517225@qq.com>
2017-02-20 16:56:23 +0800

    views和model的大调整，模块化，修复了test3.html的模块

--- DJANGO1/urls.py
+++ DJANGO1/urls.py
@@ -19,14 +19,10 @@ Including another URLconf
 
 from django.conf.urls import url
 from django.contrib import admin
-from shuang.views import index
-from shuang.views import normal
-from shuang.views import search_allrows
-from shuang.views import spider_search
-from shuang.views import formula_query
-from shuang.views import define_formula
-from shuang.views import all_basic_ext
-from shuang.views import all_basic_ext1
+from shuang.src.basic.basic_controller import index, normal, search_allrows, all_basic_ext1, all_basic_ext
+from shuang.src.pachong.controller import spider_search
+from shuang.src.formula.formula_controller import formula_query, define_formula
+
 from shuang.views import test
 
 urlpatterns = [
@@ -35,8 +31,8 @@ urlpatterns = [
     url(r'^shuang/normal$',normal), #基本查询页面
     url(r'^shuang/searchforbasic$',search_allrows), #查询所有的列 查询转跳
     url(r'^shuang/spider$',spider_search),#爬虫页面
-   # url(r'^shuang/cal_baisc$',all_basic_ext), #充填全局表的扩展表
-   # url(r'^shuang/cal_baisc1$',all_basic_ext1),#充填事实表的扩展表
+    url(r'^shuang/cal_baisc$',all_basic_ext), #充填全局表的扩展表
+    url(r'^shuang/cal_baisc1$',all_basic_ext1),#充填事实表的扩展表
     url(r'^shuang/test$',test),#测试bootstrap用
     url(r'shuang/gongshi$',formula_query),
     url(r'shuang/dy_gongshi$',define_formula),
