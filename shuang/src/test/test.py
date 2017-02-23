5ec4edab7ff8d7dbd7884a8e7c9a65faccb2ad14
foliage11111 <378517225@qq.com>
2017-02-24 00:26:16 +0800

    修改了 test 的引入 django 的程序

--- shuang/src/test/test.py
+++ shuang/src/test/test.py
@@ -2,9 +2,12 @@
 __author__ = 'zr'
 import re
 import datetime
+import os
 import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DJANGO1.settings")
 
 django.setup()
+
 from shuang.src.formula.formula_model import ssq_formula
 
 # a1=' class="blue">\xe6\x9f\xa5\xe7\x9c\x8b\xe8\xaf\xa6\xe6\x83\x85</a></td></tr><tr><td>2016034</td><td>2016-03-27 21:30(\xe6\x97\xa5)</td><td class="red">03&nbsp;15&nbsp;21&nbsp;22&nbsp;23&nbsp;28</td><td class="blue">15</td><td>112</td><td>4</td><td>4</td><td>2</td><td>2</td><td><a href="/kaijiang/ssq/2016034.html" target="_blank" class="blue">\xe6\x9f\xa5\xe7\x9c\x8b\xe8\xaf\xa6\xe6\x83\x85</a></td></tr><tr><td>2016033</td><td>2016-03-24 21:30(\xe5\x9b\x9b)</td><td class="red">06&nbsp;17&nbsp;18&nbsp;20&nbsp;27&nbsp;29</td><td class="blue">15</td><td>117</td><td>5</td><td>3</td><td>2</td><td>1</td><td><a href="/kaijiang/ssq/2016033.html" target="_blank" class="blue">\xe6\x9f\xa5\xe7\x9c\x8b\xe8\xaf\xa6\xe6\x83\x85</a>'
