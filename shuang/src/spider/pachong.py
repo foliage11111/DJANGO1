005f11ce8f7913ed5544eab8f15c1301448ebe45
foliage11111 <378517225@qq.com>
2017-10-07 17:06:57 +0800

    爬虫的修正，更换了新的网站，更健全的数据输入查询条件。 补全了2013年的数据。

--- shuang/src/spider/pachong.py
+++ shuang/src/spider/pachong.py
@@ -3,6 +3,7 @@ __author__ = 'zr'
 import  urllib
 import urllib2
 import re
+from shuang.src.basic.basic_model import TSsqShishibiao
 import json
 import time
 # from urllib import urlopen
@@ -10,6 +11,7 @@ import time
 
 def get_web(num):
     '''
+    最开始的从woying.com获取数据
     [[T返回期数,红球,蓝球],]
     :param num:
     :return:T返回期数,红球,蓝球
@@ -43,6 +45,9 @@ def get_web(num):
     if response.getcode() == 200:  #返回200，表示正常？  3打头的一般是重定向，301是永久重定向，302是临时重定向,404表示网页不存在，403禁止访问.500系列是响应过长
         print response.info()
         print req1.headers
+    else:
+        print 'web is down！'
+        return [],'web is down'
     cont=response.read()
     # print cont
     # ssq_list=re.subn(r'(\r\n)+|\s+','',cont)
@@ -61,3 +66,113 @@ def get_web(num):
     #print response.read()
     #urllib.urlretrieve(url3,"c:\\Users\\zr\\Desktop\\ssq_test1.txt")  ###上面抓取网页，抓取以后要下载网页
 
+
+
+def get_web_datachart_last():
+    '''
+    从datachart.500.com网站获取最近一个最大的值
+    :return:字符串的 list,比如 [2017117]
+    '''
+    url3 = 'http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=' + '30'
+    header1 = {}
+    header1[
+        'User-Agent'] = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"
+
+    req1 = urllib2.Request(url3)  ##req1 = urllib2.Request(urll,'',header1)
+    req1.add_header('User-Agent', header1['User-Agent'])  ##需要区别上面加header的方法，这里需要指定header里面某个关键字，然后增加字符串内容
+    # req1.set_proxy('27.38.152.195:9797','http') #设置代理
+    response = urllib2.urlopen(req1)
+    if response.getcode() == 200:  #返回200，表示正常？  3打头的一般是重定向，301是永久重定向，302是临时重定向,404表示网页不存在，403禁止访问.500系列是响应过长
+        print response.info()
+        print req1.headers
+    else:
+        print 'web is down！'
+        return ['web is down']
+    cont=response.read()
+    #print 'cont    '+cont
+    ssq_list=re.subn(r'(\r+|\n+)+\s+','',cont) ##去除重复的内容
+    # print ssq_list
+    table1=re.findall(r'tbody.*?tbody',ssq_list[0]) ##找出主table
+    ssq_list=re.findall(r'<td align="center">.*?</tr>',table1[0]) ##找出每一期的记录分段
+    print ssq_list
+    test2 = re.findall(r'<td align="center">(.*?)</td>',ssq_list[-1])#期数
+
+    ssq=['20'+test2[0].strip()]#期数里面不知道为啥就是有空格，去不掉，肯定是我对正则的理解不够
+    ssq = map(int, ssq)  # 字符串转数字
+    print ssq
+    return ssq
+
+def get_web_datachart_ajax(num):
+    '''
+    从datachart.500.com网站获取所有值
+    考虑了最大期数等问题后，如果输入的批次过大，则从网页取最后一个作为 from to 的内容
+    [[T返回期数,红球1,红球2,红球3,红球4,红球5,红球6,蓝球],]
+    :param num:
+    :return:[[T返回期数,红球1,红球2,红球3,红球4,红球5,红球6,蓝球],]
+    '''
+    #url3 = 'http://www.woying.com/kaijiang/ssqls/'+num+'.html'
+
+    first_ball = TSsqShishibiao.objects.last()
+    print first_ball.num,first_ball.get_all_balls_byList()
+    last_ball=get_web_datachart_last()
+    if first_ball and last_ball:#正常情况下应该都是从第一个 url3开始
+        print 'first_ball', first_ball.num
+        print 'last_ball', last_ball[0]
+        print num
+        if first_ball.num+int(num)>int(last_ball[0]): #如果自己的最后一个加上批次大于了网页的最大值，则用from 我的第一个 to 网页的最后一个
+            url3='http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=all&from=' + \
+                 str(first_ball.num-10)[2:] + '&to=' + str(last_ball[0])[2:] + '&jumpsrc=http://datachart.500.com/ssq/'
+        else:##如果最后一个加上批次小于网页的最大值,则使用 from 我的最后一个 to 我的最后一个加上+batch
+            url3 = 'http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=all&from=' + \
+            str(first_ball.num)[2:] + '&to=' + str(int(first_ball.num+ int(100)))[2:] + '&jumpsrc=http://datachart.500.com/ssq/'
+    else: #任意一个为空，则默认拿100个
+        url3 = 'http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=' + '100'
+    print url3
+
+    #f = urllib.urlopen(url='file:/D:\\myapplesapple_id.txt')  打开本地文件
+    #f = urllib.urlopen(url='ftp://python:read@www.*****.com/')  打开ftp
+    #
+    # r.add_data(data) 向请求添加数据。如果请求是HTTP请求，则方法改为‘POST’。data是向指定url提交的数据，要注意该方法不会将data追教导之前已经设置的任何数据上，而是使用现在的data替换之前的。
+    # r.add_header(key, val) 向请求添加header信息，key是报头名，val是报头值，两个参数都是字符串。
+    # r.addunredirectedheader(key, val) 作用基本同上，但不会添加到重定向请求中。
+    # r.set_proxy(host, type) 准备请求到服务器。使用host替换原来的主机，使用type替换原来的请求类型。
+
+    ###以上是使用urlopen直接打开的方法，下面说的是使用request对象的方法，主要目的是为了增加header，标准的urlopen也支持data和proxy，
+    header1 = {}
+    header1['User-Agent'] = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"
+
+    req1 = urllib2.Request(url3)   ##req1 = urllib2.Request(urll,'',header1)
+    req1.add_header('User-Agent',header1['User-Agent']) ##需要区别上面加header的方法，这里需要指定header里面某个关键字，然后增加字符串内容
+    # req1.set_proxy('27.38.152.195:9797','http') #设置代理
+    response=urllib2.urlopen(req1)
+    #print html1.read()
+    # print html1.read().decode("gbk").encode("utf-8")
+
+     # print response.info()  #返回映射对象，该对象带有与url关联的信息，对HTTP来说，返回的服务器响应包含HTTP包头。
+            # 对于FTP来说，返回的报头包含'content-length'。对于本地文件，返回的报头包含‘content-length’和'content-type'字段。
+            #要注意的是，类文件对象u以二进制模式操作。如果需要以文本形式处理响应数据，则需要使用codecs模块或类似方式解码数据。
+    #print  response.getcode()
+    if response.getcode() == 200:  #返回200，表示正常？  3打头的一般是重定向，301是永久重定向，302是临时重定向,404表示网页不存在，403禁止访问.500系列是响应过长
+        #print response.info()
+        print req1.headers
+    else:
+        print 'web is down！'
+        return [],'web is down'
+    cont=response.read()
+    #print cont
+    ssq_list=re.subn(r'(\r+|\n+)+\s+','',cont) ##去除重复的内容
+    table1=re.findall(r'tbody.*?tbody',ssq_list[0]) ##找出主table
+    ssq_list=re.findall(r'<td align="center">.*?</tr>',table1[0]) ##找出每一期的记录分段
+    #print ssq_list
+    ssq1 =[]
+    #print ssq_list
+    for char in ssq_list:#制作成为list然后塞入一个list#
+        test2 = re.findall(r'<td align="center">(.*?)</td>',char)#期数
+        test3 =re.findall(r'<td class="chartBall01">(.*?)</td>',char)#红球
+        test4 =re.findall(r'<td class="chartBall02">(.*?)</td>',char)#蓝球
+        ssq=['20'+test2[0].strip()]+test3+test4#期数里面不知道为啥就是有空格，去不掉，肯定是我对正则的理解不够
+        ssq = map(int, ssq)  # 字符串转数字
+        print ssq
+        ssq1.append(ssq)
+
+    return ssq1,response.getcode()
--- shuang/src/spider/spdier_controller.py
+++ shuang/src/spider/spdier_controller.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from shuang.src.basic.basic_model import TSsqShishibiao
 from shuang.src.basic.basic_controller import cal_shishibiao_ext
-from shuang.src.spider.pachong import get_web
+from shuang.src.spider.pachong import get_web,get_web_datachart_ajax
 
 __author__ = 'zr'
 
@@ -20,24 +20,40 @@ def spider_search(request):
     try:
         if request.method=='POST':# if 'ssq_num' in request.GET:#GET是一个dict，使用文本框的name作为key #在这里需要做一个判断，是否存在提交数据，以免报错
             num= request.POST['batch']
-            batch,return_code[0]=get_web(num) #按个抓取双色球，并返回列表和查询网页状态
-            for i in batch:
-                ssq_list= TSsqShishibiao.objects.filter(num=i[0])#确实事实表里面是否已近各有了这个期数
+            print num
+            batch,return_code[0]=get_web_datachart_ajax(num) #按个抓取双色球，并返回列表和查询网页状态
+            if batch:
+                for i in batch:
+                    ssq_list= TSsqShishibiao.objects.filter(num=i[0])#确实事实表里面是否已近各有了这个期数
+                    print '新 ssq 是否存在？',i
+                    if not ssq_list.exists() : #如果是空的说明不存在,我之前还判断里面的[0]，其实一点都没有，所以报错了
+                        print '插入新 ssq', i[0], '0'
+                        ssq_tmp=TSsqShishibiao()
+                        ssq_tmp.chushihua(i) #列表初始化事实表，并关联总表外键
+                        ssq_tmp.save()
+                        ssq_ext=cal_shishibiao_ext(ssq_tmp)
+                        #ssq_ext.num=ssq_tmp
+                        ssq_ext.save()
+                        return_list.append(i)
+                if return_list:
+                    print '事实表插入完成',return_list
+                    return_code[0] = '事实表插入完成'
+                else:
+                    return_code[0] = '没有更新的数据，未发生数据写入'
+            else:
+                print 'web get nothing'
+                return_code[0] = 'form fail !!!!!!!!!!!!!!!!!!!'
+            # print '为什么不插入 ext 表？'
+            # for j in return_list:#先全部插入事实表，再把插入成功的数据生成对应的事实表
+            #     ssq_tmp2=TSsqShishibiao()
+            #     ssq_tmp2.chushihua(j)
+            #     ssq_ext=cal_shishibiao_ext(ssq_tmp2)
+            #     ssq_ext.save()
+            #     print  '插入外键表',j
 
-                if not ssq_list.exists() : #如果是空的说明不存在,我之前还判断里面的[0]，其实一点都没有，所以报错了
-                    print '有值才插入', ssq_list, '0'
-                    ssq_tmp=TSsqShishibiao()
-                    ssq_tmp.chushihua(i) #列表初始化事实表，并关联总表外键
-                    ssq_tmp.save()
-                    return_list.append(i)
-            print 'cross ssb'
-            for j in return_list:#先全部插入事实表，再把插入成功的数据生成对应的事实表
-                ssq_tmp2=TSsqShishibiao()
-                ssq_tmp2.chushihua(j)
-                ssq_ext=cal_shishibiao_ext(ssq_tmp2)
-                ssq_ext.save()
     except Exception:
-        return_code[0]='form fail'
+        print Exception
+        return_code[0]='form fail !!!!!!!!!!!!!!!!!!!'
 
     t = loader.get_template('basic/spider_search.html')
     Context={'ssq1':return_list,'message':return_code[0],'batch':num,'ssq_len':len(return_list)}
