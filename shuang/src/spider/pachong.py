0b25eb89dff494a66f4bc7604c969675f0b90a5b
foliage11111 <378517225@qq.com>
2017-10-08 15:13:07 +0800

    优化了不同情况下的算法

--- shuang/src/spider/pachong.py
+++ shuang/src/spider/pachong.py
@@ -4,6 +4,7 @@ import  urllib
 import urllib2
 import re
 from shuang.src.basic.basic_model import TSsqShishibiao
+from django.db.models import Max
 import json
 import time
 # from urllib import urlopen
@@ -102,9 +103,9 @@ def get_web_datachart_last():
     print ssq
     return ssq
 
+
 def get_web_datachart_ajax(num):
     '''
-    从datachart.500.com网站获取所有值
     考虑了最大期数等问题后，如果输入的批次过大，则从网页取最后一个作为 from to 的内容
     [[T返回期数,红球1,红球2,红球3,红球4,红球5,红球6,蓝球],]
     :param num:
@@ -112,19 +113,21 @@ def get_web_datachart_ajax(num):
     '''
     #url3 = 'http://www.woying.com/kaijiang/ssqls/'+num+'.html'
 
-    first_ball = TSsqShishibiao.objects.last()
-    print first_ball.num,first_ball.get_all_balls_byList()
+    #first_ball = TSsqShishibiao.objects.last()
+    #print 'first_ball.num',first_ball.num,'first_ball.get_all_balls_byList()',first_ball.get_all_balls_byList()
+    #其实用这个 last 的方法也可以，他是根据主键后排序的来取值的。其实也很快。
+    first_ball =int(TSsqShishibiao.objects.all().aggregate(Max('num')).get('num__max'))
+
     last_ball=get_web_datachart_last()
     if first_ball and last_ball:#正常情况下应该都是从第一个 url3开始
-        print 'first_ball', first_ball.num
-        print 'last_ball', last_ball[0]
-        print num
-        if first_ball.num+int(num)>int(last_ball[0]): #如果自己的最后一个加上批次大于了网页的最大值，则用from 我的第一个 to 网页的最后一个
+        print 'first_ball',first_ball
+        print 'last_ball',last_ball[0]
+        if first_ball+int(num)>=int(last_ball[0]): #如果自己的最后一个加上批次大于了网页的最大值，则用from 我的第一个 to 网页的最后一个
             url3='http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=all&from=' + \
-                 str(first_ball.num-10)[2:] + '&to=' + str(last_ball[0])[2:] + '&jumpsrc=http://datachart.500.com/ssq/'
+                 str(first_ball)[2:] + '&to=' + str(last_ball[0])[2:] + '&jumpsrc=http://datachart.500.com/ssq/'
         else:##如果最后一个加上批次小于网页的最大值,则使用 from 我的最后一个 to 我的最后一个加上+batch
             url3 = 'http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=all&from=' + \
-            str(first_ball.num)[2:] + '&to=' + str(int(first_ball.num+ int(100)))[2:] + '&jumpsrc=http://datachart.500.com/ssq/'
+            str(first_ball)[2:] + '&to=' + str(int(first_ball+ int(100)))[2:] + '&jumpsrc=http://datachart.500.com/ssq/'
     else: #任意一个为空，则默认拿100个
         url3 = 'http://datachart.500.com/ssq/zoushi/newinc/jbzs_redblue.php?expect=' + '100'
     print url3
@@ -151,21 +154,23 @@ def get_web_datachart_ajax(num):
      # print response.info()  #返回映射对象，该对象带有与url关联的信息，对HTTP来说，返回的服务器响应包含HTTP包头。
             # 对于FTP来说，返回的报头包含'content-length'。对于本地文件，返回的报头包含‘content-length’和'content-type'字段。
             #要注意的是，类文件对象u以二进制模式操作。如果需要以文本形式处理响应数据，则需要使用codecs模块或类似方式解码数据。
+
     #print  response.getcode()
     if response.getcode() == 200:  #返回200，表示正常？  3打头的一般是重定向，301是永久重定向，302是临时重定向,404表示网页不存在，403禁止访问.500系列是响应过长
-        #print response.info()
-        print req1.headers
+        print response.info()
+       # print req1.headers
     else:
         print 'web is down！'
         return [],'web is down'
     cont=response.read()
     #print cont
     ssq_list=re.subn(r'(\r+|\n+)+\s+','',cont) ##去除重复的内容
+    # print ssq_list
     table1=re.findall(r'tbody.*?tbody',ssq_list[0]) ##找出主table
     ssq_list=re.findall(r'<td align="center">.*?</tr>',table1[0]) ##找出每一期的记录分段
     #print ssq_list
     ssq1 =[]
-    #print ssq_list
+    # print ssq_list
     for char in ssq_list:#制作成为list然后塞入一个list#
         test2 = re.findall(r'<td align="center">(.*?)</td>',char)#期数
         test3 =re.findall(r'<td class="chartBall01">(.*?)</td>',char)#红球
